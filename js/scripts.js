a4df1fa2c7145f35bdbfd20f103be57121a46964
NikkitaTorres <nikkitatorres@yahoo.com>
2023-10-18 14:36:58 -0700

    age verification site for amusement park rides

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,34 @@
-window.onload = function() {
-  // we get the form element
-  const form = document.querySelector("form");
-  // we create an event handler for the form's submission
-  form.onsubmit = function(event) {
+function hideResultsAndError() {
+  document.getElementById("error-message").setAttribute("class", "hidden");
+  document.getElementById("swings").setAttribute("class", "hidden");
+  document.getElementById("coaster").setAttribute("class", "hidden");
+  document.getElementById("tower").setAttribute("class", "hidden");
+  document.getElementById("sorry").setAttribute("class", "hidden");
+}
+
+window.onload = function(){
+  document.querySelector("form").onsubmit = function(event) {
+    hideResultsAndError();
     event.preventDefault();
-
-    // we access two HTML elements, the drink menu and the message to underaged folks,
-    // and add the hidden class to those elements;
-    // doing this clears results before displaying new ones, which
-    // allows the user to submit the form again and again, and
-    // see new results.
-    let drinkMenu = document.getElementById("drinks");
-    drinkMenu.setAttribute("class", "hidden");
-    let under21Message = document.getElementById("under-21");
-    under21Message.setAttribute("class", "hidden");
-    
-    // we gather the age value
     const age = parseInt(document.querySelector("input#age").value);
+    const height = parseInt(document.querySelector("input#height").value);
 
-    // we check if the age is greater than 21.
-    if (age >= 21) {
-      drinkMenu.removeAttribute("class");
-    } 
+    if (age && height) {
+     if (age >= 12 && height >= 60) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+        document.getElementById("tower").removeAttribute("class");
+      } else if (age >= 12 || height >= 48 && age >= 6) {
+        document.getElementById("swings").removeAttribute("class");
+        document.getElementById("coaster").removeAttribute("class");
+      } else if (age >= 6) {
+        document.getElementById("swings").removeAttribute("class");
+      } else {
+        document.getElementById("sorry").removeAttribute("class");
+      }
+      
+    } else {
+      document.getElementById("error-message").removeAttribute("class");
+    }
   };
-};
+}
